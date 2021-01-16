bc31d8310323f9559e76e0c412ea73eff69b08c0
Enzo Jimenez-Soto <enzojimenezsoto@gmail.com>
2021-01-15 20:38:24 -0800

    added Https

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ const App = () => {
 	const [searchValue, setSearchValue] = useState('');
 
 	const getMovieRequest = async (searchValue) => {
-		const url = `http://www.omdbapi.com/?s=${searchValue}&apikey=e67bc1e7`;
+		const url = `https://www.omdbapi.com/?s=${searchValue}&apikey=e67bc1e7`;
 
 		const response = await fetch(url);
 		const responseJson = await response.json();
